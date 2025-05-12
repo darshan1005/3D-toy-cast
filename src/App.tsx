db4ac32d859e19dcc526aff27a611dfdc5282549
darshan1005 <battuladarshan7838@gmail.com>
2025-05-12 17:42:14 +0530

    Hero section code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
 import MainPage from './pages/MainPage'
-import Hero from '@components/Hero'
-import Select from '@components/Select'
-import Testimonies from '@components/Testimonies'
-import Footer from '@components/Footer'
 
 const App = () => {
   console.log('App component rendered')
   return (
-    <div>
+    <>
       <MainPage />
-    </div>
+    </>
   )
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,182 @@
-import React from 'react'
-import { Box, Card, CardContent, Grid, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
+import Logo from '../assets/Logo.svg'
+import BikeImage from '../assets/Bike.svg'
+import CarImage from '../assets/Car.svg'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useState } from 'react';
+
+const likeCount = 700;
 
 const Hero = () => {
-  return <Box></Box>
+  const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(likeCount);
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+    setCount(liked ? count - 1 : count + 1);
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'red',
+        padding: 3,
+      }}>
+      <Box sx={{
+        backgroundColor: 'white',
+        padding: 2,
+        borderRadius: 3,
+        height: '600px'
+      }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            mb: 2
+          }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }} >
+            <Box
+              sx={{
+                width: '40px',
+                height: '40px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              component={'img'}
+              src={Logo}
+            />
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: 600,
+                backgroundColor: 'red',
+                width: 'max-content',
+                color: 'white',
+                padding: 1,
+                fontSize: '20px',
+              }} >
+              3D TOY CAST
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1,
+              cursor: 'pointer',
+            }}
+            onClick={handleLikeClick}
+          >
+            <FavoriteIcon sx={{ color: liked ? 'red' : 'black' }} />
+            <Typography variant='h5' color='black' fontWeight={'bold'}>{count}</Typography>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            overflow: 'hidden'
+          }}
+        >
+          <Box
+            sx={{
+              position: 'relative',
+              background: 'linear-gradient(to bottom, black, white)',
+              width: '20%',
+              height: '540px',
+              overflow: 'visible',
+            }}
+          >
+            <Box
+              component={'img'}
+              src={BikeImage} alt="bike image"
+              sx={{
+                position: 'absolute',
+                bottom: '-20px',
+                right: '-50px',
+              }} />
+          </Box>
+          <Box
+            display={'flex'}
+            flexDirection={'column'}
+            justifyContent={'space-between'}
+            alignItems={'flex-start'}
+
+            sx={{
+              width: '60%',
+              px: 2,
+              height: '500px',
+              overflow: 'hidden'
+            }}
+          >
+            <Box>
+              <Typography
+                variant='h1'
+                sx={{
+                  fontSize: '100px',
+                  fontWeight: 600,
+                }}
+              >
+                3D Printed Models & <mark>Collectables</mark>
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                sx={{
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  color: 'grey',
+                  width: '95%'
+                }}
+              >
+                Transform your collection with our high-resolution 3D diecast toys and detailed model frames.
+              </Typography>
+            </Box>
+            <Button
+              size='small'
+              sx={{
+                backgroundColor: 'black',
+                color: 'white',
+                border: '2px solid black',
+                alignSelf: 'flex-end',
+                padding: '10px 20px',
+                fontWeight: 'bold',
+                '&:hover': {
+                  backgroundColor: 'white',
+                  color: 'black',
+                },
+              }}
+            >
+              Order Now !
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              position: 'relative',
+              background: 'linear-gradient(to bottom, red, white)',
+              width: '20%',
+              height: '540px',
+              overflow: 'visible',
+            }}>
+            <Box
+              component={'img'}
+              src={CarImage} alt="car image"
+              sx={{
+                position: 'absolute',
+                bottom: '0px',
+                top: '40%',
+                left: '-100px',
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box >
+  )
 }
 
 export default Hero
