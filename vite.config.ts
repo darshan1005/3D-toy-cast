a51baba0714f8833ba38d3eedf02d4c2239f0554
BHASKARVIJAYKUMAR12 <BHASKARVIJAYOFFICIAL2023@GMAIL.COM>
2025-05-26 15:16:16 +0530

    Added Content Management System setup files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'static/admin',
+          dest: '' // copies to dist/admin
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
