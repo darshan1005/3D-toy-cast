e66c5af40bbe465cd36f33c5bb9f79f4e7d65d8c
BHASKARVIJAYKUMAR12 <BHASKARVIJAYOFFICIAL2023@GMAIL.COM>
2025-05-26 16:08:44 +0530

    updated vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'static/admin',
-          dest: '' // copies to dist/admin
-        }
-      ]
-    })
   ],
   resolve: {
     alias: {
