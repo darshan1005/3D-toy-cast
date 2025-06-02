295562414492a90ecabffeb1fa70c2ce68547140
darshan1005 <battuladarshan7838@gmail.com>
2025-06-02 11:14:25 +0530

    Guide changes

--- src/components/custom/Guide.tsx
+++ src/components/custom/Guide.tsx
@@ -1,6 +1,65 @@
-import { useState } from 'react'
-import { Alert, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Button, Stack } from '@mui/material'
 import PopupHOC from './HOC/PopupHOC'
+import LazyImage from './LazyImage'
+import Image1 from '../../assets/GuideImages/image (1).png'
+import Image2 from '../../assets/GuideImages/image (2).png'
+import Image3 from '../../assets/GuideImages/image (3).png'
+import Image4 from '../../assets/GuideImages/image (4).png'
+import Image5 from '../../assets/GuideImages/image (5).png'
+import Image6 from '../../assets/GuideImages/image (6).png'
+import Image7 from '../../assets/GuideImages/image (7).png'
+import Image8 from '../../assets/GuideImages/image (8).png'
+import Image9 from '../../assets/GuideImages/image (9).png'
+import Image10 from '../../assets/GuideImages/image (10).png'
+import Image11 from '../../assets/GuideImages/image (11).png'
+
+const GuidImgArray = [
+  {
+    id: 1,
+    src: Image1
+  },
+  {
+    id: 2,
+    src: Image2
+  },
+  {
+    id: 3,
+    src: Image3
+  },
+  {
+    id: 4,
+    src: Image4
+  },
+  {
+    id: 5,
+    src: Image5
+  },
+  {
+    id: 6,
+    src: Image6
+  },
+  {
+    id: 7,
+    src: Image7
+  },
+  {
+    id: 8,
+    src: Image8
+  },
+  {
+    id: 9,
+    src: Image9
+  },
+  {
+    id: 10,
+    src: Image10
+  },
+  {
+    id: 11,
+    src: Image11
+  }
+]
 
 const Guide = () => {
   const [open, setOpen] = useState(false)
@@ -30,9 +89,14 @@ const Guide = () => {
       >
         Quick Guide
       </Button>
-      <PopupHOC open={open} onClose={handleClose} title="Quick Guide" centerTitle>
-        <Typography variant="h5">
-          <Alert color='info' severity='info'>Comming soon</Alert></Typography>
+      <PopupHOC open={open} width={'80%'} onClose={handleClose} title="Quick Guide" centerTitle>
+        <Stack spacing={2}>
+          {GuidImgArray.map((i) => (
+            <React.Fragment key={i.id}>
+              <LazyImage src={i.src} />
+            </React.Fragment>
+          ))}
+        </Stack>
       </PopupHOC>
     </>
   )
